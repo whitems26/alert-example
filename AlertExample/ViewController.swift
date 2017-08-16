c806349f84331a66e1be9ae602be796030fbd7c2
whitems26 <whitems26@naver.com>
2017-08-16 16:33:43 +0900

    add alert example

--- AlertExample/ViewController.swift
+++ AlertExample/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func openActionSheet(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Notice",
+                                      message: "Message", preferredStyle: .actionSheet)
+        
+        let cancelBtn = UIAlertAction(title: "Cancel", style: .cancel, handler: {(UIAlertAction) ->Void in
+            print("on Cancel!!!")})
+        
+        
+        alert.addAction(cancelBtn)
+        
+        self.present(alert, animated: true, completion: {() -> Void in
+        print("Show action sheet")})
+        
+    }
+    
+    @IBAction func showAlert(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
